d9d81e23c3beb2024e8adfe69d70d1c115b4cc86
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-28 16:47:29 -0700

    Added beforeCreate hook and authenticateBasic method

--- auth/models/index.js
+++ auth/models/index.js
@@ -12,24 +12,32 @@ const DATABASE_URL = process.env.NODE_ENV === 'test' ?
 
 const sequelize = new Sequelize(DATABASE_URL);
 
-// Create a Sequelize model
+
+// Create a Sequelize model with beforeCreate hook
 const UsersModel = sequelize.define('User', {
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
+  username: { type: DataTypes.STRING, allowNull: false },
+  password: { type: DataTypes.STRING, allowNull: false },
+});
+
+UsersModel.beforeCreate(async (user) => {
+  let complexity = 5;
+  user.password = await bcrypt.hash(user.password, complexity);
 });
 
 UsersModel.authenticateBasic = async function (username, password) {
+  /*
+  Now that we finally have username and password, let's see if it's valid
+  1. Find the user in the database by username
+  2. Compare the plaintext password we now have against the encrypted password in the db
+      - bcrypt does this by re-encrypting the plaintext password and comparing THAT
+  3. Either we're valid or we throw an error
+*/
   let user = await this.findOne({ where: { username } });
 
   if (user) {
-    return await bcrypt.compare(password, user.password);
-  } 
+    let valid = await bcrypt.compare(password, user.password);
+    return { valid, user };
+  }
 };
 
 module.exports = {
