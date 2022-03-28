13c36b403a0b5341bb3f82422aad6ef6f80943ea
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-28 16:46:38 -0700

    Added sequelize drop and sync to test

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -3,10 +3,15 @@
 const supertest = require('supertest');
 const { app } = require('../src/app');
 const request = supertest(app);
+const { sequelize } = require('../auth/models');
+
+beforeAll(() => sequelize.sync());
+
+afterAll(() => sequelize.drop());
 
 describe('Testing API authentication', () => {
-  test('Testing that we can create a user record', () => {
-    let response = request.post('/signup').send({
+  test('Testing that we can create a user record', async () => {
+    let response = await request.post('/signup').send({
       username: 'Jeffrey',
       password: 'supersecret',
     });
