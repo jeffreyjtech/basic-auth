d37b652c871410cf62357b9e6f609f14b3f45347
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-28 18:00:32 -0700

    Removed basicAuth logic, adjusted error response info

--- auth/router.js
+++ auth/router.js
@@ -1,64 +1,38 @@
 'use strict';
 
-const base64 = require('base-64');
+// const base64 = require('base-64');
 
 const express = require('express');
+const basicAuth = require('./basic-auth');
 
 const router = express.Router();
 
 // Import UsersModel
-const { UsersModel } = require('./userModel');
+const UsersModel = require('./user-model');
 
 // Signup Route -- create a new user
 // Two ways to test this route with httpie
 // echo '{"username":"john","password":"foo"}' | http post :3000/signup
 // http post :3000/signup usernmae=john password=foo
 
-router.post('/signup', async (req, res) => {
+router.post('/signup', async (request, response, next) => {
 
   try {
-    console.log(req.body);
-    const record = await UsersModel.create(req.body);
-    res.status(200).json(record);
+    const record = await UsersModel.create(request.body);
+    response.status(201).json(record);
   } catch (error) {
-    console.error(error.message);
-    res.status(403).send('Invalid Login');
+    error.status = 400;
+    error.message = 'Error creating user account';
+    next(error);
   }
 });
 
 // Signin Route -- login with username and password
 // test with httpie
 // http post :3000/signin -a john:foo
-router.post('/signin', async (req, res) => {
+router.post('/signin', basicAuth, async (request, response, next) => {
 
-  /*
-    req.headers.authorization is : "Basic sdkjdsljd="
-    To get username and password from this, take the following steps:
-      - Turn that string into an array by splitting on ' '
-      - Pop off the last value
-      - Decode that encoded string so it returns to user:pass
-      - Split on ':' to turn it into an array
-      - Pull username and password from that array
-  */
-
-  let basicHeaderParts = req.headers.authorization.split(' ');  // ['Basic', 'sdkjdsljd=']
-  let encodedString = basicHeaderParts.pop();  // sdkjdsljd=
-  let decodedString = base64.decode(encodedString); // "username:password"
-  let [username, password] = decodedString.split(':'); // username, password
-
-  try {
-    let { valid, user } = UsersModel.authenticateBasic(username, password);
-    if (valid) {
-      res.status(200).json(user);
-    }
-    else {
-      throw new Error('Invalid User');
-    }
-  } catch (error) {
-    console.error(error.message);
-    res.status(403).send('Invalid Login');
-  }
-
+  response.status(200).json(request.user);
 });
 
 module.exports = router;
