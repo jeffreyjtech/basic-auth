767cf17c6941e3a1b9cb8921e1380156e7c1c87b
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-28 17:03:08 -0700

    Separated sequelize init logic into database.js

--- auth/userModel.js
+++ auth/userModel.js
@@ -3,15 +3,7 @@
 // I'm assuming there will only be one user schema and interface, so in this file I will create the model AND the interface class, then export the class.
 
 const bcrypt = require('bcrypt');
-const { Sequelize, DataTypes } = require('sequelize');
-
-// Assign the DB URL conditionally
-const DATABASE_URL = process.env.NODE_ENV === 'test' ?
-  'sqlite:memory' : // Either to sqlite:memory in testing mode
-  process.env.DATABASE_URL || 'postgresql://localhost:5432'; // Or process.env.DATABASE_URL in deployment
-
-const sequelize = new Sequelize(DATABASE_URL);
-
+const { sequelize, DataTypes } = require('../src/database');
 
 // Create a Sequelize model with beforeCreate hook
 const UsersModel = sequelize.define('User', {
@@ -41,6 +33,5 @@ UsersModel.authenticateBasic = async function (username, password) {
 };
 
 module.exports = {
-  sequelize,
   UsersModel,
 };
