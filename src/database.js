84c69738b0080651deca5a838307158a85ab6a53
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-28 18:26:43 -0700

    Added Heroku Postgres options to Sequelize call

--- src/database.js
+++ src/database.js
@@ -7,7 +7,15 @@ const DATABASE_URL = process.env.NODE_ENV === 'test' ?
   'sqlite:memory' : // Either to sqlite:memory in testing mode
   process.env.DATABASE_URL || 'postgresql://localhost:5432'; // Or process.env.DATABASE_URL in deployment
 
-const sequelize = new Sequelize(DATABASE_URL);
+const sequelize = new Sequelize(DATABASE_URL ,
+  { // this argument is required for Heroku Postgres to deploy properly
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });  
 
 module.exports = {
   sequelize,
